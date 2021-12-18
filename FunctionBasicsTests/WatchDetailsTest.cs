41cb538657cf2ff04c1b8092e10ae6b843810c82
jagadeep <developerjagadeep@outlook.com>
2021-12-18 18:09:23 +0530

    first commit

---
+++ FunctionBasicsTests/WatchDetailsTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Internal;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Primitives;
+using Microsoft.Extensions.Logging.Abstractions;
+
+namespace FunctionBasicsTests
+{
+    public class WatchDetailsTest
+    {
+        [Fact]
+        public void RunMethodShouldReturn200OnValidModel()
+        {
+
+            var req = new DefaultHttpRequest(new DefaultHttpContext())
+            {
+                Query = new QueryCollection(new Dictionary<string, StringValues>() { { "model", "hgf" } })
+            };
+
+            var logger = NullLoggerFactory.Instance.CreateLogger("Null Logger");
+
+            var res = FunctionBasics.WatchDetails.Run(req, logger);
+
+            res.Wait();
+
+            Assert.IsAssignableFrom<OkObjectResult>(res.Result);
+
+
+
+
+        }
+    }
+}
